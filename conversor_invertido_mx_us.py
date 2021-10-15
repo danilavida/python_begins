128e4a8dfaad84dd450e2f7233ddc5470e675a12
Daniel Davila <danielrojo9822@gmail.com>
2021-10-15 17:46:22 -0500

    concluido

--- conversor_invertido_mx_us.py
+++ conversor_invertido_mx_us.py
@@ -1,6 +1,7 @@
 dolares = input("Cuantos dolares tienes ? : ")
 dolares = float(dolares)
-valor_peso = 1
-pesos = dolares / valor_peso
-pesos = round(dolares, 2)
-pesos = str(dolares)
+valor_peso = 21
+pesos = dolares * valor_peso
+pesos = round(pesos, 2)
+pesos = str(pesos)
+print("Tienes $" + pesos + " pesos")
